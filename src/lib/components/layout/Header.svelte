00f6bfbc57d9f7ef47bfbda13ef41b9ac069617b
Giorgio <giorgio.duminuco@hiop.io>
2025-07-17 12:35:50 +0200

    Added user menu mock and managed header in responsive

--- src/lib/components/layout/Header.svelte
+++ src/lib/components/layout/Header.svelte
@@ -1,16 +1,21 @@
 <script lang="ts">
-  import Menu from "./Menu.svelte";
-  import Icon from "@iconify/svelte";
+  import Menu from "../menu/Menu.svelte";
+  import UserMenu from "../menu/UserMenu.svelte";
 </script>
 
 <header class="px-2 shadow-md md:px-6">
   <a href="#main-content" class="sr-only focus:not-sr-only">
     Skip to main content
   </a>
-  <div class="flex items-center justify-between">
-    <a href="/" aria-label="Go to homepage">
+  <div class="flex items-center justify-between gap-2">
+    <a href="/" aria-label="Go to homepage" class="order-2 lg:order-1">
       <img src="/logo.webp" alt="Pack logo" class="max-w-20" />
     </a>
-    <Menu />
+    <div class="order-3 lg:order-2">
+      <Menu />
+    </div>
+    <div class="order-1 lg:order-3">
+      <UserMenu />
+    </div>
   </div>
 </header>
---
+++ src/lib/components/menu/UserMenu.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import Icon from "@iconify/svelte";
+</script>
+
+<div class="flex items-center gap-3">
+  <Icon icon="twemoji:flag-united-states" width="24" />
+  <div
+    class="border-coral-500 flex h-6 w-6 items-center justify-center overflow-hidden rounded-full border-2"
+  >
+    <img
+      src="/user-placeholder.jpg"
+      alt="User"
+      class="h-full w-full object-cover"
+    />
+  </div>
+</div>
